2cda2b2d3857cb09a7648e9ad13eb67b64f961d4
jinjinfan <cindyfan23@gmail.com>
2022-12-20 22:03:03 -0500

    Initial commit

---
+++ ch2_a_tour_of_rust/greatest_common_divisor/src/main.rs
@@ -0,0 +1,20 @@
+use std::str::FromStr;
+use std::env;
+
+fn main() {
+	let mut numbers : Vec<u64> = Vec::new();
+	for arg in env::args().skip(1) {
+		numbers.push(u64::from_str(&arg).expect("error parsing argument"));
+	}
+	if numbers.len() == 0 {
+		eprintln!("Usage: gcd NUMBER ...");
+		std::process::exit(1);
+	}
+
+	let mut d = numbers[0];
+	for m in &numbers[1..] {
+		d = greatest_common_divisor::gcd(d, *m);
+	}
+
+	println!("The greatest common divisor of {:?} is {}", numbers, d);
+}
